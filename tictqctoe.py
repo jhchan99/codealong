8cab2be07d419327f3f2fe2e1366608c26f9f1e6
James Chan <jhchan99@gmail.com>
2022-01-15 19:05:28 -0700

    revised

--- tictqctoe.py
+++ tictqctoe.py
@@ -15,27 +15,20 @@ def main():
         Thanks them for playing
         return: None
     '''
-    # assign/get the first player
     player = next_player("")
-    # create a board
     board = create_board()
-    # loop if there isn't a winner or if the game isn't a draw
-        while not is_winner and not is_draw:
-        # display the board
-            display_board(board)
-        # allow the player to make a move
-            make_move(player, board)
-        # pick the next player
-            player = next_player(player)
-    # display the final board
+    while not is_winner and not is_draw:
         display_board(board)
-    # show message for winner and thanks for playing
-        if is_winner == "x":
-            print("Congrats player x!")
-        elif is_winner == "y":
-            print("Congrats player o!")
-        else:
-            print("Thanks for playing!")
+        make_move(player, board)
+        player = next_player(player)
+    display_board(board)
+    if is_winner == "x":
+        print("Congrats player x!")
+    elif is_winner == "y":
+        print("Congrats player o!")
+    else:
+        print("Thanks for playing!")
+
 
 def create_board():
     ''' Creates a list that holds the spots on the board
@@ -68,6 +61,7 @@ def is_draw(board):
             return False
     return True
 
+
 def is_winner(board):
     ''' return: True if someone won, False if there is no winner '''
     return (board[0]==board[1]==board[2]or
@@ -88,11 +82,12 @@ def make_move(player, board):
     chosen = int(input(f"{player}'s choice (0-8)"))
     board[chosen - 1] = player    
 
+
 def next_player(current):
     ''' return: x if current is o, otherwise x '''
     if current == "" or current=="0":
         return "x"
 
-# run main if this has been called from the command line
+
 if __name__ == "__main__":
     main()
