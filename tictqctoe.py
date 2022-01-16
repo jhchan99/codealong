b01fbc968aa26843240b1320a57dc0335702f911
James Chan <jhchan99@gmail.com>
2022-01-15 18:39:45 -0700

    finished project

--- tictqctoe.py
+++ tictqctoe.py
@@ -16,23 +16,26 @@ def main():
         return: None
     '''
     # assign/get the first player
-    
+    player = next_player("")
     # create a board
-    create_board()
-    display_board(board)
+    board = create_board()
     # loop if there isn't a winner or if the game isn't a draw
-    if not is_winner() and not is_draw:
-        pass
+        while not is_winner and not is_draw:
         # display the board
-
+            display_board(board)
         # allow the player to make a move
-
+            make_move(player, board)
         # pick the next player
-
+            player = next_player(player)
     # display the final board
-
+        display_board(board)
     # show message for winner and thanks for playing
-
+        if is_winner == "x":
+            print("Congrats player x!")
+        elif is_winner == "y":
+            print("Congrats player o!")
+        else:
+            print("Thanks for playing!")
 
 def create_board():
     ''' Creates a list that holds the spots on the board
@@ -64,22 +67,31 @@ def is_draw(board):
         if board[square] != "x" and board[square] != "o":
             return False
     return True
-    pass
 
 def is_winner(board):
     ''' return: True if someone won, False if there is no winner '''
-    pass
+    return (board[0]==board[1]==board[2]or
+            board[3]==board[4]==board[5]or
+            board[6]==board[7]==board[8]or
+            board[0]==board[4]==board[8]or
+            board[0]==board[3]==board[6]or
+            board[1]==board[4]==board[7]or
+            board[2]==board[5]==board[8]or
+            board[2]==board[4]==board[6])
+            
 
 def make_move(player, board):
     ''' Prompts player to select a square to play
         Assigns the player to that board location if it is a legal move
         return: None
     '''
-    pass      
+    chosen = int(input(f"{player}'s choice (0-8)"))
+    board[chosen - 1] = player    
 
 def next_player(current):
     ''' return: x if current is o, otherwise x '''
-    pass
+    if current == "" or current=="0":
+        return "x"
 
 # run main if this has been called from the command line
 if __name__ == "__main__":
