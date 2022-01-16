77e343fff4a37fd88ca2239ec0003d78066c9778
James Chan <jhchan99@gmail.com>
2022-01-15 18:05:58 -0700

    updated is_draw

--- tictqctoe.py
+++ tictqctoe.py
@@ -3,6 +3,9 @@ Solo Checkpoint 02
 Author: Bro. Hayes
 '''
 
+from tkinter.messagebox import RETRY
+
+
 def main():
     ''' Holds the main game loop logic
         Selects a player
@@ -13,11 +16,13 @@ def main():
         return: None
     '''
     # assign/get the first player
-    james
+    
     # create a board
-
+    create_board()
+    display_board(board)
     # loop if there isn't a winner or if the game isn't a draw
-
+    if not is_winner() and not is_draw:
+        pass
         # display the board
 
         # allow the player to make a move
@@ -27,23 +32,38 @@ def main():
     # display the final board
 
     # show message for winner and thanks for playing
-    pass
+
 
 def create_board():
     ''' Creates a list that holds the spots on the board
         It initializes the positions with the numbers for the person to pick
         return: the board as a list
     '''
-    pass
+    board = []
+    for square in range(9):
+        board.append(square + 1)
+        return board
+
 
 def display_board(board):
     ''' Displays the current board
         return: None
     '''
-    pass
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print("=-=")
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print("=-=")
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
+
 
 def is_draw(board):
     ''' return: True if board is a draw, False if board is still playable '''
+    for square in range[9]:
+        if board[square] != "x" and board[square] != "o":
+            return False
+    return True
     pass
 
 def is_winner(board):
